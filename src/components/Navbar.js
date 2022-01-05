92eee7136ffbd56bc84fdbee0e1ffe618f3c661d
dieore <orefici.diego@gmail.com>
2022-01-05 03:31:45 -0300

    Changing some styles on nav.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 const Navbar = () => {
     return (
-        <nav className="p-3 sticky top-0">
-            <ul className="hidden sm:grid grid-cols-3 place-items-center text-white font-light">
-                <li className="hover:underline hover:underline-offset-8 hover:font-normal">About</li>
-                <li className="hover:underline hover:underline-offset-8 hover:font-normal">Skills</li>
-                <li className="hover:underline hover:underline-offset-8 hover:font-normal">Contact</li>
+        <nav className="sm:p-6 p-3 sticky top-0 bg-blue bg-opacity-20">
+            <ul className="hidden sm:grid grid-cols-3 place-items-center text-white">
+                <li className="hover:underline hover:underline-offset-8">ABOUT</li>
+                <li className="hover:underline hover:underline-offset-8">SKILLS</li>
+                <li className="hover:underline hover:underline-offset-8">CONTACT</li>
             </ul>
             <div className="flex justify-between items-center sm:hidden">
                 <h5 className="text-white font-mono">Diego Orefici</h5>
@@ -14,4 +14,4 @@ const Navbar = () => {
     )
 }
 
-export default Navbar
+export default Navbar;
