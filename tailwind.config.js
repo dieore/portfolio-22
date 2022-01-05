32e87fe44f8ede50cc2a46cb8c394c16536f264e
dieore <orefici.diego@gmail.com>
2022-01-05 03:31:25 -0300

    Tailwind colors updated.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,15 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      white: "white",
+      blue: "rgb(11, 20, 26)",
+      "light-blue": "rgb(16, 38, 65)",
+      gray: "#282726",
+      "light-gray": "#334155",
+      orange: "#fb923c"
+    },
     extend: {},
   },
   plugins: [],
