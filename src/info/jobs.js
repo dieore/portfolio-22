6f9946c76cde1ccb2ba1382288e6745d4e0b7fb3
dieore <orefici.diego@gmail.com>
2022-01-07 01:44:38 -0300

    Information to use on jobs.

---
+++ src/info/jobs.js
@@ -0,0 +1,31 @@
+export const siempresano = {
+    src: "/videos/siempresano.mp4",
+    to: "https://siempresano.com.ar",
+    message: "https://siempresano.com.ar",
+    images: ["/img/siempresanoportada.png", "/img/siempresanoform.png", "/img/siempresanopop.png"],
+    text: "This is the first project that I always like to show. It was a pleasure and also a bit hard. I have to struggle with lot of things that I didn't know and the most challenging of it, was to integrate Mercado Pago. So, I was handling people's money, and anything could go wrong. Finally, I can say, that all the tasks were completed.In about three months, the job was done.I think it took more than expected but, also because other people (designers) take part in the project.Technology stack: Loopback 3, Next JS, Material UI, Mongo DB."
+}
+
+export const soleal = {
+    src: "/videos/soleal.mp4",
+    to: "https://soleal.com.ar",
+    images: ["/img/solealportada.png", "/img/solealinfo.png", "/img/solealotra.png"],
+    text: "Soleal was my second project in 2021. In this case, my client need that customers could get appointments throught the app. It was cool to work with dates and a real challenge to make the admin works. I learned a lot for other projects that were about to come up. Technology stack: Loopback 3, Next JS, CSS, Material UI, Mongo DB.",
+    message: "This app is under maintenance. Could not be working."
+}
+
+export const pulso = {
+    src: "/videos/pulso.mp4",
+    to: "https://siempresano.com.ar",
+    message: "The app can only be access from Argentina.",
+    images: [],
+    text: "First time with React Native. Of course, I knew React, but in this project, I learned about React Router Navigation, about local storage on mobile and React Native Paper, etc. Another developer teach me several things and also started the project from scratch to do the right configuration. I also was very close in the Android process to deploy the app. Technology stack: React Native, CSS. "
+}
+
+export const dulce = {
+    src: "/videos/dulce.mp4",
+    to: "https://dulcearoma.netlify.app",
+    message: "You can login with these credentials. User: dulcearoma. Password: dulcearoma.",
+    images: ["/img/dulcecaja.png", "/img/dulcecajatwo.png", "/img/dulceprod.png"],
+    text: "I really enjoy this project. I was very comfortable and secure with the technologies that I used because I already have some experience. I could decide better ways to design the app and not to worry about problems that may come up. Technology stack: Loopback 3, React, Mongo DB, Material UI."
+}
