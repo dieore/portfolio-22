d081fb2e302f64844a384b51f70d540e333f8ea7
dieore <orefici.diego@gmail.com>
2022-01-07 17:46:32 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
             <nav className={navigationBackground}>
                 <div className="hidden sm:grid grid-cols-2 text-white">
                     <div>
-                        <button onClick={() => setShowCV(true)} className="bg-light-blue border p-3 hover:bg-light-gray cursor-pointer text-sm">WATCH MY CV</button>
+                        <button onClick={() => setShowCV(true)} className="bg-light-blue border p-3 hover:bg-light-gray cursor-pointer text-sm">CHECK MY CV</button>
                     </div>
                     <ul className="flex justify-around items-center">
                         <a href="#home" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm">HOME</a>
@@ -39,18 +39,18 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="flex justify-between sm:hidden">
-                    <button onClick={() => setShowCV(true)} className="text-white bg-light-blue border p-3 hover:bg-light-gray cursor-pointer text-sm">WATCH MY CV</button>
-                    <img onClick={() => setOpen(!open)} className="w-8" src="/img/burguer.png" />
+                    <button onClick={() => setShowCV(true)} className="text-white bg-light-blue border p-3 hover:bg-light-gray cursor-pointer text-sm">CHECK MY CV</button>
+                    <img onClick={() => setOpen(!open)} className="w-10" src="/img/burguer.png" />
                 </div>
             </nav>
             {
                 open && (
-                    <div className="fixed h-screen w-3/6 bg-blue bg-opacity-90 text-white">
-                        <ul className="grid pl-3 pt-4 h-4/6">
-                            <a onClick={() => setOpen(false)} href="#home" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm hover:font-medium">HOME</a>
-                            <a onClick={() => setOpen(false)} href="#about" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm hover:font-medium">ABOUT</a>
-                            <a onClick={() => setOpen(false)} href="#skills" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm hover:font-medium">SKILLS</a>
-                            <a onClick={() => setOpen(false)} href="#contact" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm hover:font-medium">CONTACT</a>
+                    <div className="fixed h-screen w-screen bg-blue bg-opacity-90 z-10 text-white">
+                        <ul className="grid place-items-center text-sm tracking-widest pt-4 h-4/6">
+                            <a onClick={() => setOpen(false)} href="#home" className="hover:underline hover:underline-offset-8 cursor-pointer hover:font-medium">HOME</a>
+                            <a onClick={() => setOpen(false)} href="#about" className="hover:underline hover:underline-offset-8 cursor-pointer hover:font-medium">ABOUT</a>
+                            <a onClick={() => setOpen(false)} href="#skills" className="hover:underline hover:underline-offset-8 cursor-pointer hover:font-medium">SKILLS</a>
+                            <a onClick={() => setOpen(false)} href="#contact" className="hover:underline hover:underline-offset-8 cursor-pointer hover:font-medium">CONTACT</a>
                         </ul>
                     </div>
                 )
