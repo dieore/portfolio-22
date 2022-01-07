0319a1385328cc6349c0271f7fdbb4c062d36a66
dieore <orefici.diego@gmail.com>
2022-01-07 03:44:26 -0300

    Mobile menu added.

--- src/components/Job.js
+++ src/components/Job.js
@@ -7,8 +7,8 @@ const Job = ({ app }) => {
     return (
         <div className="grid grid-cols-1 text-white md:h-80 sm:place-content-between text-justify font-light md:text-base text-sm">
             <p>{app.text}</p>
-            <button className="underline text-left hover:font-semibold md:mt-3 mt-6" onClick={() => setDemo(true)}>Watch demo</button>
-            <button className="underline text-left hover:font-semibold" onClick={() => window.open(app.to)}>Take me to the app</button>
+            <button className="underline text-left outline-none hover:font-semibold md:mt-3 mt-6" onClick={() => setDemo(true)}>Watch demo</button>
+            <button className="underline text-left outline-none hover:font-semibold" onClick={() => window.open(app.to)}>Take me to the app</button>
             <div className="grid grid-cols-1 md:place-items-start place-items-center mt-6 md:gap-0 gap-6 md:grid-cols-3">
                 {
                     app.images.map(i => (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import Modal from "./Modal";
 
-const Navbar = () => {
-    const onScrollDown = "sm:p-6 p-3 sticky top-0 bg-blue bg-opacity-90";
-    const onScrollTop = "sm:p-6 p-3 sticky top-0";
+const onScrollDown = "sm:p-6 p-3 sticky top-0 bg-blue bg-opacity-90";
+const onScrollTop = "sm:p-6 p-3 sticky top-0";
 
+const Navbar = () => {
     const [navigationBackground, setNavigationBackground] = useState(onScrollTop);
     const [showCV, setShowCV] = useState(false);
+    const [open, setOpen] = useState(false);
 
     const handleScroll = () => {
         const show = window.scrollY > 310;
@@ -26,22 +27,34 @@ const Navbar = () => {
     return (
         <>
             <nav className={navigationBackground}>
-                <ul className="hidden sm:grid grid-cols-2 text-white">
+                <div className="hidden sm:grid grid-cols-2 text-white">
                     <div>
                         <button onClick={() => setShowCV(true)} className="bg-light-blue border p-3 hover:bg-light-gray cursor-pointer text-sm">WATCH MY CV</button>
                     </div>
-                    <div className="flex justify-around items-center">
+                    <ul className="flex justify-around items-center">
                         <a href="#home" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm">HOME</a>
                         <a href="#about" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm">ABOUT</a>
                         <a href="#skills" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm">SKILLS</a>
                         <a href="#contact" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm">CONTACT</a>
-                    </div>
-                </ul>
-                <div className="flex justify-center sm:hidden">
-                    <img className="w-8" src="/img/burguer.png" />
+                    </ul>
+                </div>
+                <div className="flex justify-between sm:hidden">
+                    <button onClick={() => setShowCV(true)} className="text-white bg-light-blue border p-3 hover:bg-light-gray cursor-pointer text-sm">WATCH MY CV</button>
+                    <img onClick={() => setOpen(!open)} className="w-8" src="/img/burguer.png" />
                 </div>
             </nav>
-
+            {
+                open && (
+                    <div className="fixed h-screen w-3/6 bg-blue bg-opacity-90 text-white">
+                        <ul className="grid pl-3 pt-4 h-4/6">
+                            <a onClick={() => setOpen(false)} href="#home" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm hover:font-medium">HOME</a>
+                            <a onClick={() => setOpen(false)} href="#about" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm hover:font-medium">ABOUT</a>
+                            <a onClick={() => setOpen(false)} href="#skills" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm hover:font-medium">SKILLS</a>
+                            <a onClick={() => setOpen(false)} href="#contact" className="hover:underline hover:underline-offset-8 cursor-pointer text-sm hover:font-medium">CONTACT</a>
+                        </ul>
+                    </div>
+                )
+            }
             <Modal
                 open={showCV}
                 setStateFromComponent={setShowCV}
